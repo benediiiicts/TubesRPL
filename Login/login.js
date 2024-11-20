616223049c433e028c1c96013305fadbae39bda6
STANISLAUS NATHAN <i22092@ftis>
2024-11-20 15:59:19 +0700

    Penambahan script log in dan penyusunan ulang directory

---
+++ Login/login.js
@@ -0,0 +1,18 @@
+function checkLogin(){
+
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+
+    const tempPassword = "Admin123";
+    if (password === tempPassword){
+        window.location.href = "../Home/home.html";
+    } else{
+        alert("Password Anda Salah.");
+    }
+}
+
+document.querySelector("form").addEventListener("submit", event => {
+    event.preventDefault();
+    
+    checkLogin();
+});
