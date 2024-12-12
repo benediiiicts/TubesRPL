000269dea8d55830b92cf59746a43a4e7ca6a48b
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2024-12-13 02:37:03 +0700

    update mapping login, struktur java

---
+++ portal-ta/portal-ta/src/main/java/com/rpl/portal_ta/data/NilaiSidang.java
@@ -0,0 +1,10 @@
+package com.rpl.portal_ta.data;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class NilaiSidang{
+    
+}
