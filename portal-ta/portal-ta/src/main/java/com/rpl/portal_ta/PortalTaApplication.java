bc1a2bc59d873732f597a5192fb22ae0848ccf6e
BENEDICTUS KRISTIADI A W <benedictuskristiadi@gmail.com>
2024-12-13 01:34:41 +0700

    update struktur

--- portal-ta/portal-ta/src/main/java/com/rpl/portal_ta/PortalTaApplication.java
+++ portal-ta/portal-ta/src/main/java/com/rpl/portal_ta/PortalTaApplication.java
@@ -2,8 +2,9 @@ package com.rpl.portal_ta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PortalTaApplication {
 
 	public static void main(String[] args) {
